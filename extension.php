c2348fd8e3c3219c8ab39c972e9fd4c19e87c1ce
Mickael PAPAMICA Asseline <mickael@papamica.com>
2026-05-16 03:42:11 +0200

    ✨ feat: only for connected users

--- extension.php
+++ extension.php
@@ -67,6 +67,10 @@ Articles:
 	}
 
 	public function renderNavButton(): string {
+		if (!FreshRSS_Auth::hasAccess()) {
+			return '';
+		}
+
 		$svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">'
 			. '<path d="M9.5 2A2.5 2.5 0 0 1 12 4.5v15a2.5 2.5 0 0 1-4.96-.46 2.5 2.5 0 0 1-1.65-4.56A2.5 2.5 0 0 1 2 12a2.5 2.5 0 0 1 2.39-2.48 2.5 2.5 0 0 1 1.65-4.56A2.5 2.5 0 0 1 9.5 2Z"/>'
 			. '<path d="M14.5 2A2.5 2.5 0 0 0 12 4.5v15a2.5 2.5 0 0 0 4.96-.46 2.5 2.5 0 0 0 1.65-4.56A2.5 2.5 0 0 0 22 12a2.5 2.5 0 0 0-2.39-2.48 2.5 2.5 0 0 0-1.65-4.56A2.5 2.5 0 0 0 14.5 2Z"/>'
